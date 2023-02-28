9426d33f1a729a2400713b259e8794782e623f10
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-02-28 17:57:32 -0300

    key
    entendendo sobre keywords

--- 3_Avançando no React/react_advanced/src/components/ListRender.js
+++ 3_Avançando no React/react_advanced/src/components/ListRender.js
@@ -2,12 +2,26 @@ import {useState} from 'react';
 
 const ListRender = () => {
     const [list] = useState(['Mateus', 'Pedro', 'Lisa','Luana']);
+
+    const [users] = useState([
+      {id:1, name:'Mateus', age:31}, 
+      {id:454844, name:'Pedro', age:26},
+      {id:254874, name:'Luana', age:29},  
+    ]);
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
             ))}
+        </ul>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.id} - {user.name} - {user.age}
+            </li>
+          ))}
         </ul>
     </div>
   )
