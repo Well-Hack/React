8e397df7f0719347ff1fd6ba4d3cb73eea9f92a5
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-03 16:01:18 -0300

    css em componentes
    css

--- 4_React_CSS/css/src/App.js
+++ 4_React_CSS/css/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import MyComponents from './components/MyComponents';
 
 function App() {
   return (
     <div className="App">
+      {/*CSS global */}
       <h1>React com CSS</h1>
+      {/*CSS components */}
+      <MyComponents/>
+      <p>Este paragrafo é do App.js {'\n'}</p>
+      {/*Inline Css*/}
+      <p style={{backgroundcolor: 'black',}}>
+        Este elemento foi estilizado de forma inline (dentro da tag)</p>
     </div>
   );
 }
---
+++ 4_React_CSS/css/src/components/MyComponents.js
@@ -0,0 +1,14 @@
+import './MyComponents.css';
+
+const MyComponents = () => {
+  return (
+    <div>
+        <h1>Isso é um componente</h1>
+        <p>Paragrafo do Componentes</p>
+        <p className='my-comp-paragraph'>Este também é do componente</p>
+
+    </div>
+  )
+}
+
+export default MyComponents
