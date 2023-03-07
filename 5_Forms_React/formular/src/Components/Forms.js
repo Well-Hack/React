05a4e89412e7cb59d772d6b06ab9e3aa59f4a903
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-07 16:57:15 -0300

    label
    label

--- 5_Forms_React/formular/src/Components/Forms.js
+++ 5_Forms_React/formular/src/Components/Forms.js
@@ -6,13 +6,21 @@ const Forms = () => {
         <h2>Formulario</h2>
         <form>
             <div>
-                <label htmlFor="name" target='name'>Nome:
+                <label htmlFor="name">Nome:
                 <input type="text" name='name' id='name' placeholder= 'Digite o seu nome'/></label>
 
                 <input type="submit" name="enviar" id="submit" value='enviar'/>
                 <input type="reset" value='Resetar'/>
             </div>
         </form>
+         {/*Label com input*/}
+      <form>
+          <label>
+            <span>E-mail</span>
+            <input type='email' name='email' placeholder='Digite seu E-mail'/>
+           </label>
+            <input type="submit" value='Enviar' />
+      </form>
 
     </div>
   )
