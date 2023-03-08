a3e7a7dc57d21737cebbde0ebb9ff7db2f8fcc59
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-08 16:23:25 -0300

    Envio de Form
    Envio de formularios

--- 5_Forms_React/formular/src/Components/Forms.js
+++ 5_Forms_React/formular/src/Components/Forms.js
@@ -10,11 +10,17 @@ const Forms = () => {
     const handleName = (e) => {
         setName(e.target.value);
     };
-      console.log(name);
-      console.log(email);
+      //console.log(name);
+      //console.log(email);
+
+      const handleSubmit = (event) => {   
+            event.preventDefault();
+            console.log('Enviando os Dados');
+            console.log(name, email)
+          };
 
   return (
-    <div>
+      <div>
         <h2>Formulario</h2>
         <form>
             <div>
@@ -25,8 +31,20 @@ const Forms = () => {
                 <input type="reset" value='Resetar'/>
             </div>
         </form>
-         {/*Label com input*/}
-      <form>
+         {/*Label com input e simplificação de manip. de state*/}
+        <form>
+          <label>
+            <span>E-mail</span>
+            <input type='email' name='email' placeholder='Digite seu E-mail' onChange={(e) => setEmail(e.target.value)}/>
+           </label>
+            <input type="submit" value='Enviar' />
+      </form>
+      {/*Envio de Form */}
+      <form onSubmit={handleSubmit}>
+      <div>
+                <label htmlFor="name">Nome:
+                <input type="text" name='name' id='name' placeholder= 'Digite o seu nome'onChange={(e) => setName(e.target.value)}/></label>
+            </div>
           <label>
             <span>E-mail</span>
             <input type='email' name='email' placeholder='Digite seu E-mail' onChange={(e) => setEmail(e.target.value)}/>
