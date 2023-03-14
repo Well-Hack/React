e15d9a2cc5db6da298d9189b374d4e8298ba0722
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-14 15:24:15 -0300

    secret_word

---
+++ 6_Secret_Word/secretword/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+    país: ["Brasil","Camarões", "Argentina", "Mexico", "Inglaterra", "Russia", "Canada"],
+    fruta: ["Banana", "Abacaxi", "Laranja", "Melancia", "Abacate", "Amora", "Tangerina"],
+    corpo: ["Dedo", "Ombro", "Perna", "Cintura", "Olhos", "Dentes","Lingua"],
+    comida: ["Arroz", "Pastel", "Coxinha", "Hamburguer", "Regrigerante", "Suco", "Bolo"],
+    marcas: ["Nike", "Adidas", "Ferrari", "Fanta", "Riachuello", "Netflix","Amazon"],
+    cores: ["Azul", "Vermelho", "Verde", "Amarelo", "Laranja", "Branco", "Roxo"], 
+};
