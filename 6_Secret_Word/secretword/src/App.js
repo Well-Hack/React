e4fc803500328a1c8aed495e4509c234d5a12167
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-21 18:16:23 -0300

    att
    attt

--- 6_Secret_Word/secretword/src/App.js
+++ 6_Secret_Word/secretword/src/App.js
@@ -1,8 +1,22 @@
+//CSS
 import './App.css';
+//React
+import{useCallback, useEffect, useState} from "react";
 
+//data
+import {wordsList} from "./data/words";
+
+//components
 import StartScreen from './components/StartScreen';
 
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+]
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
   return (
     <div className="App">
     <StartScreen/>
