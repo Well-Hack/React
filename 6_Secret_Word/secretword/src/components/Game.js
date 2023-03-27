90941e3b31cae79a21c2592e0a52f648f47dfca9
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-27 18:12:51 -0300

    Estruturando componentes do jogo

--- 6_Secret_Word/secretword/src/components/Game.js
+++ 6_Secret_Word/secretword/src/components/Game.js
@@ -2,8 +2,24 @@ import "./Game.css";
 
 const Game = ({verifyLetter}) => {
   return (
-    <div><h1>Game</h1>
-    <button onClick={verifyLetter}>Finalizar o jogo</button>
+    <div className = "game">
+      <p className= "points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h3 className="tip">
+        Dica sobre a palabra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="Letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p> Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required/>
+          <button>Jogar!</button>
+        </form>
+      </div>
     </div>
   )
 }
