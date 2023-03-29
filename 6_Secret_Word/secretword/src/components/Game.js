f088a5291c19f4410a2243c3ddbe3a44a00c1019
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-29 18:30:39 -0300

    focus
    colocando focus

--- 6_Secret_Word/secretword/src/components/Game.js
+++ 6_Secret_Word/secretword/src/components/Game.js
@@ -1,14 +1,19 @@
-import { useState} from "react";
+import { useState, useRef} from "react";
 import "./Game.css";
 
 const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score
 }) => {
     const [letter, setLetter] = useState("");
+    const letterInputRef = useRef(null)
 
     const handleSubmit = (e) => {
       e.preventDefault();
 
       verifyLetter(letter)
+
+      setLetter("");
+
+      letterInputRef.current.focus();
     };
   return (
     <div className = "game">
@@ -34,7 +39,7 @@ const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters
       <div className="letterContainer">
         <p> Tente advinhar uma letra da palavra:</p>
         <form onSubmit={handleSubmit}>
-          <input type="text" name="letter" maxLength="1" required onChange={(e) => setLetter(e.target.value)} value={letter}/>
+          <input type="text" name="letter" maxLength="1" required onChange={(e) => setLetter(e.target.value)} value={letter} ref={letterInputRef}/>
           <button>Jogar!</button>
         </form>
       </div>
