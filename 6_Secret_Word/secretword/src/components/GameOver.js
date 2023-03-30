dd5ba4bf6d15b5dbeb4c1665d51017c52afd325a
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-30 18:39:55 -0300

    pntuações

--- 6_Secret_Word/secretword/src/components/GameOver.js
+++ 6_Secret_Word/secretword/src/components/GameOver.js
@@ -1,8 +1,9 @@
 import "./GameOver.css"
 
-const GameOver = ({retry}) => {
+const GameOver = ({retry, score}) => {
   return (
-    <div><h1>GameOver</h1>
+    <div><h1>Fim de Jogo</h1>
+    <h2>A sua pontuação foi: <span>{score}</span> </h2>
     <button onClick={retry}>Reiniciar o jogo</button>
     </div>
   )
